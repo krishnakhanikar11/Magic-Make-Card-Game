701ab7693c1f775fa3c1b2b0eb1a337af5c68fdd
Krishna Khanikar <krishna.khanikar2019@vitstudent.ac.in>
2021-12-23 22:51:16 +0530

    initial commit

---
+++ src/components/SingleCard.js
@@ -0,0 +1,27 @@
+import './SingleCard.css'
+
+function SingleCard({card,handleChoice,flipped,disabled}) {
+
+    const handleClick = () => {
+        if(!disabled){
+            handleChoice(card)}
+    }
+
+   
+    return (
+      <div className='card'>
+        <div className={flipped ? "flipped" : ""}>
+          <img className='front' src={card.src} alt='card front'></img>
+          <img
+            className='back'
+            src='/img/cover.png'
+            onClick={handleClick}
+            alt='card back'
+          ></img>
+        </div>
+      </div>
+    )
+}
+
+export default SingleCard
+
